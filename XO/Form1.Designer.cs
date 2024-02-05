e38cf24dc5f2ba5248c8fa01f49f0e08e25cf4e8
neh111 <n0504119296@gmail.com>
2024-02-05 15:43:15 +0200

    first commit

---
+++ XO/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace XO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(603, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "איקס עיגול";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(929, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "בחר גודל לוח";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(844, 166);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(69, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(27, 209);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1185, 395);
+            this.panel1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.Location = new System.Drawing.Point(176, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "בחר סוג שחקן";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "X",
+            "O"});
+            this.comboBox1.Location = new System.Drawing.Point(158, 161);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button1.Location = new System.Drawing.Point(79, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 44);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "רענן";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1246, 616);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ XO/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XO
+{
+    public partial class Form1 : Form
+    {
+        static bool x = false;
+        static int numericValue = 3;
+        static Button[,] matBoard =new Button[numericValue,numericValue];
+        public Form1()
+        {
+            InitializeComponent();
+            ReStart();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+            numericValue = (int)numericUpDown1.Value;
+            matBoard= new Button[numericValue, numericValue];
+            for (int i = 0; i < numericValue*numericValue; i++)
+            {
+                    Button b = new Button();
+                    b.Text = "?";
+                    b.Location = new Point(861-i% numericValue*60, 48+ i / numericValue*60);
+                    b.Size = new Size(60, 60);
+                    matBoard[i / numericValue, i % numericValue] = b;
+                    panel1.Controls.Add(b);
+                    b.Click += ClickButtonXO;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string chosen = comboBox1.Text;
+            if (chosen == "X")
+                x = true;
+            else
+                x = false;
+        }
+        private void ClickButtonXO(object sender, EventArgs e)
+        {
+            if (x)
+            {
+                ((Button)sender).Text="X";
+                x = false;
+            }
+            else
+            {
+                ((Button)sender).Text="O";
+                x = true;
+            }
+            string str = ((Button)sender).Text;
+            if (IsWin(str))
+            {
+                MessageBox.Show(str+" is the winner!!");
+            }
+            ((Button)sender).Enabled = false;
+        }
+
+        private bool IsWin(string now)
+        {
+            return IsWinLines(now) || IsWinCoulmns(now) || IsWinDiagonals(now);
+        }
+        private bool IsWinLines(string now)
+        {
+            bool flag = false;
+            for (int i = 0; i < numericValue && (!flag); i++)
+            {
+                for (int j = 0; j < numericValue; j++)
+                {
+                    if (matBoard[i, j].Text != now)
+                        break;
+                    if (j == numericValue-1)
+                        flag = true;
+                }
+            }
+            return flag;
+        }
+        private bool IsWinCoulmns(string now)
+        {
+            bool flag = false;
+            for (int i = 0; i < numericValue && (!flag); i++)
+            {
+                for (int j = 0; j < numericValue; j++)
+                {
+                    if (matBoard[j,i].Text != now)
+                        break;
+                    if (j == numericValue-1)
+                        flag = true;
+                }
+            }
+            return flag;
+        }
+        private bool IsWinDiagonals(string now)
+        {
+            bool flag = false;
+            for (int i = 0; i < numericValue && (!flag); i++)
+            {
+                if (matBoard[i, i].Text != now)
+                    break;
+                else
+                {
+                    if (i == numericValue - 1)
+                        return true;
+                }
+            }
+            int j = numericValue - 1;
+            for (int i = 0; i < numericValue; i++)
+             {
+                if (matBoard[i, j].Text != now)
+                    return false;
+                j--;
+             }
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+            ReStart();
+        }
+
+        private void ReStart()
+        {
+            numericValue = 3;
+            numericUpDown1.Value=numericValue;
+            for (int i = 0; i < numericValue * numericValue; i++)
+            {
+                Button b = new Button();
+                b.Text = "?";
+                b.Location = new Point(861 - i % numericValue * 60, 48 + i / numericValue * 60);
+                b.Size = new Size(60, 60);
+                matBoard[i / numericValue, i % numericValue] = b;
+                panel1.Controls.Add(b);
+                b.Click += ClickButtonXO;
+            }
+        }
+    }
+}
